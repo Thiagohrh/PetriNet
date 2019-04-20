91b43cc9850b4f46b672f06358c822af8f1674e1
Thiago Henrique <thiagohrh@gmail.com>
2019-04-19 22:17:46 -0300

    FloodFill Algorithm Implemented
    It makes sure you can walk on every cell of the map.

--- PetriProject/Scripts/CellularAutomata/CAutomata.gd
+++ PetriProject/Scripts/CellularAutomata/CAutomata.gd
@@ -27,6 +27,16 @@ func start_map_creation():
 	#Test to see if it can access any position through expressions like:
 	#map[2][3] or whatever.... that will make things easier.
 	GenerateMap()
+	
+	#Checks if the map has been created correctly, using the flood fill algorithm.
+	flood_fill_check()
+	if flood_fill_check():
+		print("Flood fill say its ok!")
+	else:
+		print("Flood fill say its NOT OKAY! Time to redo the map...")
+		while !flood_fill_check():
+			GenerateMap()
+			pass
 	#In order to actually spawn the map....
 	SpawnSprites()
 	
@@ -140,3 +150,85 @@ func SpawnSprites():
 
 func get_map():
 	return map
+
+func flood_fill_check():
+	#Applies the flood fill algorithm in order to check if every walkable cell is acessible from a starting position.
+	#Remember: 1 is a wall, 0 is walkable cell.
+	
+	#First, get the first cell to the left to start with....
+	var start_pos = get_first_avaliable_position()
+	#Now create a dictionary that checks if a particular cell has been checked already or not.
+	var visited_dict = {}
+	#Check how many walkable tiles are there.
+	var walkable_tiles_amount = 0
+	for x in range(width):
+		for y in range(height):
+			#Go through all the cells and put a false flag on all of them...
+			visited_dict[Vector2(x,y)] = false
+			if map[x][y] == 0:
+				walkable_tiles_amount += 1
+	
+	#Create a list that keeps track of possible cells to check...
+	var cells_to_check = Array()
+	
+	#Adds to the end of that list the start_pos, so that it can start the chain reaction.
+	cells_to_check.push_back(start_pos)
+	
+	#Start the check loop.
+	while !cells_to_check.empty():
+		#Gets the first one.
+		var cell_coord = cells_to_check.front()
+		#If it is a walkable tile, and i havent checked it yet...
+		if map[cell_coord.x][cell_coord.y] == 0 and visited_dict[cell_coord] == false:
+			#Checks it as already investigated.
+			visited_dict[cell_coord] = true
+			
+			#Gets all possible neighbours directly in its side and add it to the list
+			#Gets above neighborhood
+			if map[cell_coord.x][cell_coord.y - 1] == 0 and visited_dict[Vector2(cell_coord.x, cell_coord.y - 1)] == false:
+				cells_to_check.push_back(Vector2(cell_coord.x, cell_coord.y - 1))
+				pass
+			#Gets below neighborhood
+			if map[cell_coord.x][cell_coord.y + 1] == 0 and visited_dict[Vector2(cell_coord.x, cell_coord.y + 1)] == false:
+				cells_to_check.push_back(Vector2(cell_coord.x, cell_coord.y + 1))
+				pass
+			
+			#Gets right neighborhood
+			if map[cell_coord.x + 1][cell_coord.y] == 0 and visited_dict[Vector2(cell_coord.x + 1, cell_coord.y)] == false:
+				cells_to_check.push_back(Vector2(cell_coord.x + 1, cell_coord.y))
+				pass
+			
+			#Gets left neighborhood
+			if map[cell_coord.x - 1][cell_coord.y] == 0 and visited_dict[Vector2(cell_coord.x - 1, cell_coord.y)] == false:
+				cells_to_check.push_back(Vector2(cell_coord.x - 1, cell_coord.y))
+				pass
+			
+			#After getting all the possible neighbors...its done.
+			pass
+		
+		#Done everything it has to do with this cell...so lets pop the front vector and proceed to the next.
+		cells_to_check.pop_front()
+		#And proceed with the while untill all possible cells have been walked and marked as visited.
+		pass
+	
+	var visited_amount = 0
+	#Having checked all that, count how many cells i've visited....
+	for coord in visited_dict:
+		if visited_dict[coord] == true:
+			visited_amount += 1
+			pass
+	
+	#After all that, makes the final check...
+	if visited_amount == walkable_tiles_amount:
+		return true
+	else:
+		return false
+
+func get_first_avaliable_position():
+	for x in range(map.size()):
+		for y in range(map[0].size()):
+			if map[x][y] == 0:
+				return Vector2(x,y)
+			pass
+	
+	return null
