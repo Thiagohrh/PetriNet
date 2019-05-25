8a6dcd274b700ea4e6bf968f0b62ff8237d97a1b
Thiago Henrique <thiagohrh@gmail.com>
2019-05-25 18:41:14 -0300

    Cost is calculated using Cellular Automata.
    Its not exactly a generic behaviour (its only a 30 by 30 matrix that is allowed here), but it works and looks kindda nice.

--- PetriProject/Scripts/CellularAutomata/CostCellAutomata.gd
+++ PetriProject/Scripts/CellularAutomata/CostCellAutomata.gd
@@ -34,31 +34,47 @@ func smooth_map():
 	randomize()
 	for x in range(cost_matrix.size()):
 		for y in range(cost_matrix.size()):
-			if GetSurroundingWallCost(x, y) >= 24:
-				var random_chance = randi() % 5
-				if random_chance == 0:
-					cost_matrix[x][y] = 4
-				else:
-					cost_matrix[x][y] = 3
-				pass
-			elif GetSurroundingWallCost(x, y) >= 16:
-				var random_chance = randi() % 5
-				if random_chance == 0:
-					cost_matrix[x][y] = 3
-				else:
-					cost_matrix[x][y] = 2
-				pass
-			elif GetSurroundingWallCost(x, y) < 16:
-				var random_chance = randi() % 5
-				if random_chance == 0:
-					cost_matrix[x][y] = 2
-				else:
-					cost_matrix[x][y] = 1
-				pass
+			if randi() % 2:
+				treat_cell_neighboors(x,y);
 			pass
 	pass
 
-
+func treat_cell_neighboors(gridX, gridY):
+	var cell_cost = cost_matrix[gridX][gridY]
+	
+	for neighbourX in range(gridX - 1, gridX + 2):
+		for neighbourY in range(gridY - 1, gridY + 2):
+			if neighbourX >= 0 && neighbourX < 30 && neighbourY >= 0 && neighbourY < 30:
+				if neighbourX != gridX || neighbourY != gridY:
+					
+					if cost_matrix[gridX][gridY] == 1 and randi() % 10 < 1:
+						cost_matrix[neighbourX][neighbourY] = 1
+						pass
+					elif cost_matrix[gridX][gridY] == 4 and randi() % 20 < 1:
+						cost_matrix[neighbourX][neighbourY] = 4
+						pass
+					
+					if cell_cost == 4 and cost_matrix[neighbourX][neighbourY] < 3:
+						cost_matrix[neighbourX][neighbourY] = 3
+						pass
+					elif cell_cost == 3 and cost_matrix[neighbourX][neighbourY] < 2:
+						cost_matrix[neighbourX][neighbourY] = 2
+						pass
+					elif cell_cost == 2 and cost_matrix[neighbourX][neighbourY] > 3:
+						cost_matrix[neighbourX][neighbourY] = 3
+						pass
+					elif cell_cost == 1 and cost_matrix[neighbourX][neighbourY] > 2:
+						cost_matrix[neighbourX][neighbourY] = 2
+						pass
+					
+					
+					#wallCost += cost_matrix[neighbourX][neighbourX]
+					pass
+				pass
+			pass
+		pass
+	
+	pass
 
 func GetSurroundingWallCost(gridX, gridY):
 	#Use this function to check how many walls are around the target tile (coordinates: x and y)
